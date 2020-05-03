5acc39c7625525c3e9fc45dff7d85ae312d0f783
Paulo Barbosa <paulohjbarbosa@gmail.com>
2020-05-02 23:52:51 -0300

    Alterando Status dos Itens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,12 +26,12 @@ export class AppComponent {
     }
   }
 
-  markAsDone() {
-
+  markAsDone(todo: Todo) {
+    todo.done = true;
   }
 
-  markAsUndone() {
-
+  markAsUndone(todo: Todo) {
+    todo.done = false;
   }
 
 }
