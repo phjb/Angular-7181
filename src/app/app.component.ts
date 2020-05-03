b1b239d067c2d3b54a8301c328c6b3cd0396469d
Paulo Barbosa <paulohjbarbosa@gmail.com>
2020-05-03 00:52:46 -0300

    Lendo os itens - Local Storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,7 +23,9 @@ export class AppComponent {
         Validators.maxLength(60),
         Validators.required
       ])]
-    })
+    });
+
+    this.load();
   }
 
   /* AÇÕES */
@@ -33,14 +35,17 @@ export class AppComponent {
     if (index !== -1) {
       this.todos.splice(index, 1);
     }
+    this.save();
   }
 
   markAsDone(todo: Todo) {
     todo.done = true;
+    this.save();
   }
 
   markAsUndone(todo: Todo) {
     todo.done = false;
+    this.save();
   }
   clear(){
     this.form.reset();
@@ -57,6 +62,11 @@ export class AppComponent {
   save(){
     const data = JSON.stringify(this.todos);
     localStorage.setItem('todos',data);
+  }
+
+  load(){
+    const data = localStorage.getItem('todos');
+    this.todos = JSON.parse(data);
   }
 
 }
