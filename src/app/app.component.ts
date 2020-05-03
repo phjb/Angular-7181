eb46f0240cb67f46ba481000e4ce9291685fe138
Paulo Barbosa <paulohjbarbosa@gmail.com>
2020-05-02 23:17:44 -0300

    Exibindo Dados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+
+  public todos: any[] = [];
+  public title: string = 'Minhas tarefas';
+
+  constructor() {
+    this.todos.push('passear com o doguinho');
+    this.todos.push('ir ao mercado');
+    this.todos.push('cortar o cabelo');
+
+    this.todos.push(2020);
+    this.todos.push(new Date());
+  }
+
 }
